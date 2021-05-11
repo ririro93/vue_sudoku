54a5163cf320e08e232aa13dd891b6a672ac7c39
문준하 <ririro93@gmail.com>
2021-05-12 00:40:10 +0900

    fix: data not updating bug fixed

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,32 @@ import GameInfo from '@/components/GameInfo.vue'
 import Board from '@/components/Board.vue'
 import NumberInput from '@/components/NumberInput.vue'
 
+const board = []
+const squareClasses = {}
 
+// initialize Board
+for (let i=0; i<9; i++) {
+  let rows = []
+  for (let j=0; j<9; j++) {
+    const squareId = 9 * i + j
+    squareClasses[squareId] = {
+      'border-right': false,
+      'border-bot': false,
+      'selected': false
+    }
+    rows.push('')
+    if (i === 0 && j === 0) {
+      squareClasses[squareId]['selected'] = true
+    }
+    if (j === 2 || j === 5 ) {
+      squareClasses[squareId]['border-right'] = true
+    }
+    if (i === 2 || i === 5) {
+      squareClasses[squareId]['border-bot'] = true
+    }
+  }
+  board.push(rows)
+}
 
 export default {
   name: 'App',
@@ -32,8 +57,8 @@ export default {
   data: function () {
     return {
       // for Board
-      board: [],
-      squareClasses: {},
+      board,
+      squareClasses,
       selectedSquare: 0,
     }
   },
@@ -45,47 +70,18 @@ export default {
 
     onSquareClick: function ([r, c]) {
       // change selectedSquare
-      // console.log(r, c)
       this.squareClasses[this.selectedSquare]['selected'] = false
       this.selectedSquare = 9 * r + c
       this.squareClasses[this.selectedSquare]['selected'] = true
-      console.log(this.selectedSquare)
     },
 
     onNumberClick: function (num) {
       // change selectedSquare to selectedNumber
-      console.log(num)
       const r = Math.floor(this.selectedSquare / 9)
       const c = this.selectedSquare % 9
       this.board[r][c] = num
     }
   },
-
-  created: function () {
-    // initialize Board
-    for (let i=0; i<9; i++) {
-      let rows = []
-      for (let j=0; j<9; j++) {
-        const squareId = 9 * i + j
-        this.squareClasses[squareId] = {
-          'border-right': false,
-          'border-bot': false,
-          'selected': false
-        }
-        rows.push('')
-        if (i === 0 && j === 0) {
-          this.squareClasses[squareId]['selected'] = true
-        }
-        if (j === 2 || j === 5 ) {
-          this.squareClasses[squareId]['border-right'] = true
-        }
-        if (i === 2 || i === 5) {
-          this.squareClasses[squareId]['border-bot'] = true
-        }
-      }
-      this.board.push(rows)
-    }
-  }
 }
 </script>
 
